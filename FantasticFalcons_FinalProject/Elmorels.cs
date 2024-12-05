eb553c9402a25b6123914c88e356e126c7cf46af
Luke Elmore <elmorels@mail.uc.edu>
2024-12-05 13:51:40 -0500

    Add project files.

---
+++ FantasticFalcons_FinalProject/Elmorels.cs
@@ -0,0 +1,74 @@
+﻿/*
+# Name:Luke Elmore
+# email:elmorels@mail.uc.edu
+# Assignment Title: Final Project
+# Due Date:12/10/2024
+# Course: IS 3050
+# Semester/Year:fall 2024
+# Brief Description: This Calculator takes a basic problem and solves it
+# Citations: Stacked overflow, ChatGPT for code for Calc solver,InClass_2024_11_19_IS3050_001
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Falcons_FinalProject
+{
+    public class elmorels
+    {
+        /// <summary>
+        /// Takes a math problem in a string and adds them up 
+        /// </summary>
+        /// <param name="s">put 2 numbers in </param>
+        /// <returns>The answer of math problem</returns>
+        public int Calculate(string s)
+        {
+            // Stack to store the results and signs
+            Stack<int> stack = new Stack<int>();
+            int currentNumber = 0;
+            int result = 0;
+            int sign = 1; // 1 for positive, -1 for negative
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+
+                if (char.IsDigit(c)) // If the character is a digit
+                {
+                    currentNumber = currentNumber * 10 + (c - '0');
+                }
+
+                if (c == '+' || c == '-' || c == '(' || c == ')' || i == s.Length - 1)
+                {
+                    if (c == '+' || c == '-')
+                    {
+                        result += sign * currentNumber;
+                        currentNumber = 0;
+                        sign = (c == '+') ? 1 : -1;
+                    }
+
+                    if (c == '(')
+                    {
+                        stack.Push(result);
+                        stack.Push(sign);
+                        result = 0;
+                        sign = 1; // Default sign when a new sub-expression starts
+                    }
+
+                    if (c == ')')
+                    {
+                        result += sign * currentNumber;
+                        currentNumber = 0;
+                        result *= stack.Pop(); // Multiply with the sign before the parentheses
+                        result += stack.Pop(); // Add the result before the parentheses
+                    }
+                }
+            }
+
+            result += sign * currentNumber; // Add the last number
+            return result;
+        }
+
+    }
+}
---
+++ FantasticFalcons_FinalProject/index.aspx
@@ -0,0 +1,50 @@
+﻿<!--
+# Name:Luke Elmore,Nate Hoang,Ray Happel,Zoha Iqbal
+# email:elmorels@mail.uc.edu,hoangnd@mail.uc.edu,happelrc@mail.uc.edu,iqbalza@mail.uc.edu
+# Assignment Title: Final Project
+# Due Date:12/10/2024
+# Course: IS 3050
+# Semester/Year:fall 2024
+# Brief Description: This makes a table with 2 rows and 2 columns to be able to put the problems and solutions in
+# Citations: Stacked overflow,InClass_2024_11_19_IS3050_001
+-->
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Falcons_FinalProject.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table border ="1">
+                <tr>
+                    <td>
+                        <asp:DropDownList ID="ddlSolutionSelector" runat="server" >
+                            <asp:ListItem>Basic Calculator</asp:ListItem>
+                            <asp:ListItem>Sudoku Solver</asp:ListItem>
+                            <asp:ListItem>Wildcard Matching</asp:ListItem>
+                            <asp:ListItem>Distinct Subsequences</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                    <td>
+                        <asp:Button ID="cmdClickForAnswer" runat="server" Text="Click for solution" OnClick="cmdClickForAnswer_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblSolution" runat="server" Text="Solution"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:Label ID="lblShowSolution" runat="server" Text=" "></asp:Label>
+                    </td>
+                </tr>
+               </table>
+            
+            
+        </div>
+    </form>
+</body>
+</html>
